f9cbd52edf213aea1029efc0c23f9ae5aa1da777
Phil Friderici (ephifre) <phil.friderici@ericsson.com>
2015-07-31 09:33:15 +0200

    support newer versions of Puppet v3 as well as the future parser and Puppet v4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
-require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+RSpec.configure do |config|
+  config.before :each do
+    Puppet[:parser] = 'future' if ENV['PARSER'] == 'future'
+  end
+end
