2f5287ab523921088ec012b4c3bfa9516d129de4
Phil Friderici <phil.friderici@elits.com>
2022-08-04 14:57:01 +0000

    Move OS specifics into modules hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,9 +36,9 @@ class arc (
   Boolean $create_rndrelease = true,
   Boolean $create_symlink = true,
   Boolean $install_package = true,
-  Variant[Enum['USE_DEFAULTS'], Array[String[1]]] $packages = 'USE_DEFAULTS',
-  String[1] $rndrelease_version = 'USE_DEFAULTS',
-  Variant[Enum['USE_DEFAULTS'], Stdlib::Absolutepath] $symlink_target = 'USE_DEFAULTS',
+  Optional[Array[String[1]]] $packages = undef,
+  Optional[String[1]] $rndrelease_version = undef,
+  Optional[Stdlib::Absolutepath] $symlink_target = undef,
   Boolean $manage_arc_console_icon = false,
   Boolean $arc_console_icon = false,
 ) {
@@ -48,70 +48,27 @@ class arc (
   case "${facts['os']['name']}-${facts['os']['release']['full']}" {
     /^(RedHat|CentOS)-5/: {
       $os_defaults_missing        = false
-      $packages_default           = ['libXmu.i386', 'tcl-devel.i386', 'tcsh']
-      $rndrelease_version_default = undef
-      $symlink_target_default     = '/usr/lib/libtcl8.4.so'
     }
     /^(RedHat|CentOS)-6/: {
       $os_defaults_missing        = false
-      $packages_default           = ['libXmu.i686', 'tcl-devel.i686', 'tcsh']
-      $rndrelease_version_default = undef
-      $symlink_target_default     = '/usr/lib/libtcl8.5.so'
     }
     /^(RedHat|CentOS)-7/: {
       $os_defaults_missing        = false
-      $packages_default           = ['tcsh', 'libX11.i686']
-      $rndrelease_version_default = undef
-      $symlink_target_default     = undef
     }
     /^(RedHat|CentOS)-8/: {
       $os_defaults_missing        = false
-      $packages_default           = ['tcsh', 'libX11.i686', 'libxcrypt.i686', 'libnsl.i686']
-      $rndrelease_version_default = undef
-      $symlink_target_default     = undef
     }
     /^(SLED-10|SLES-10)/: {
       $os_defaults_missing        = false
-      $packages_default           = $facts['os']['architecture'] ? {
-        'x86_64' => ['tcl-32bit', 'tcsh'],
-        default  => ['tcl', 'tcsh'],
-      }
-      $rndrelease_version_default = $facts['os']['release']['full'] ? {
-        '10.0'  => 'LMWP 2.0',
-        '10.1'  => 'LMWP 2.1',
-        '10.2'  => 'LMWP 2.2',
-        '10.3'  => 'LMWP 2.3',
-        '10.4'  => 'LMWP 2.4',
-        default => undef,
-      }
-      $symlink_target_default     = '/usr/lib/libtcl8.4.so'
     }
     /^(SLED-11|SLES-11)/: {
       $os_defaults_missing        = false
-      $packages_default           = $facts['os']['architecture'] ? {
-        'x86_64' => ['tcl-32bit', 'tcsh', 'xorg-x11-libXmu-32bit'],
-        default  => ['tcl', 'tcsh', 'xorg-x11-libXmu'],
-      }
-      $rndrelease_version_default = $facts['os']['release']['full'] ? {
-        '11.0'  => 'LMWP 3.0',
-        '11.1'  => 'LMWP 3.1',
-        '11.2'  => 'LMWP 3.2',
-        '11.3'  => 'LMWP 3.3',
-        default => undef,
-      }
-      $symlink_target_default     = '/usr/lib/libtcl8.5.so'
     }
     /^(SLED-12|SLES-12|SLED-15|SLES-15)/: {
       $os_defaults_missing        = false
-      $packages_default           = ['libXmu6-32bit', 'tcl-32bit', 'tcsh']
-      $rndrelease_version_default = undef
-      $symlink_target_default     = '/usr/lib/libtcl8.6.so'
     }
     /^(Ubuntu-12|Ubuntu-14|Ubuntu-16|Ubuntu-18|Ubuntu-20)/: {
       $os_defaults_missing        = false
-      $packages_default           = ['tcsh', 'libx11-6:i386', 'libc6:i386', 'tcl-dev']
-      $rndrelease_version_default = undef
-      $symlink_target_default     = undef
     }
     default: {
       $os_defaults_missing = true
@@ -122,36 +79,16 @@ class arc (
   # <USE_DEFAULTS vs OS defaults>
   # Check if 'USE_DEFAULTS' is used anywhere without having OS default value
   if (
-    ($packages           == 'USE_DEFAULTS' and $install_package   != true) or
-    ($rndrelease_version == 'USE_DEFAULTS' and $create_rndrelease != true) or
-    ($symlink_target     == 'USE_DEFAULTS' and $create_symlink    != true)
+    ($packages           == undef and $install_package   != true) or
+    ($rndrelease_version == undef and $create_rndrelease != true) or
+    ($symlink_target     == undef and $create_symlink    != true)
   ) and $os_defaults_missing == true {
     fail("Sorry, I don't know default values for ${facts['os']['name']}-${facts['os']['release']['full']} yet :( Please provide specific values to the arc module.") #lint:ignore:140chars
   }
   # </USE_DEFAULTS vs OS defaults>
 
-  # <assign variables>
-  # Change 'USE_DEFAULTS' to OS specific default values
-  # Convert strings with booleans to real boolean, if needed
-
-  $packages_real = $packages ? {
-    'USE_DEFAULTS' => $packages_default,
-    default        => $packages
-  }
-
-  $rndrelease_version_real = $rndrelease_version ? {
-    'USE_DEFAULTS' => $rndrelease_version_default,
-    default        => $rndrelease_version
-  }
-
-  $symlink_target_real = $symlink_target ? {
-    'USE_DEFAULTS' => $symlink_target_default,
-    default        => $symlink_target
-  }
-  # </assign variables>
-
   # <Do Stuff>
-  if ($create_rndrelease == false or $rndrelease_version_real == undef) {
+  if ($create_rndrelease == false or $rndrelease_version == undef) {
     $rndrelease_ensure = 'absent'
   } else {
     $rndrelease_ensure = 'present'
@@ -162,7 +99,7 @@ class arc (
       ensure  => $rndrelease_ensure,
       path    => '/etc/rndrelease',
       mode    => '0644',
-      content => "${rndrelease_version_real}\n",
+      content => "${rndrelease_version}\n",
     }
   }
 
@@ -183,16 +120,16 @@ class arc (
     }
   }
 
-  if ($create_symlink == true and $symlink_target_real != undef) {
+  if ($create_symlink == true and $symlink_target != undef) {
     file { 'arc_symlink':
       ensure => link,
       path   => '/usr/lib/libtcl.so.0',
-      target => $symlink_target_real,
+      target => $symlink_target,
     }
   }
 
-  if $install_package == true and $packages_real != undef {
-    package { $packages_real:
+  if $install_package == true and $packages != undef {
+    package { $packages:
       ensure => present,
     }
   }
