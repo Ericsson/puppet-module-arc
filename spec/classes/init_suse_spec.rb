d18d2cb098ff8a84a480bc29beb68eb41ade5f62
Phil Friderici <phil.friderici@elits.com>
2022-08-09 13:50:13 +0000

    Drop support for historical OS flavours
    - drop Ubuntu 12.02
    - drop SLED/SLES 10
    - drop RedHat 5
    - drop all 32 bit OS flavours

--- spec/classes/init_suse_spec.rb
+++ spec/classes/init_suse_spec.rb
@@ -1,181 +1,11 @@
-# For SLED/SLES we need individual tests for each affected minor release and
-# for both CPU architectures. Since this is not possible with rspec-puppet-facts,
-# explicit tests are necessary here. The parameters $rndrelease_version (release)
-# & $packages (architecture) are affected.
+# For SLED/SLES we need individual tests for each affected minor release.
+# Since this is not possible with rspec-puppet-facts, explicit tests are
+# necessary here. The parameters $rndrelease_version (release) is affected.
 
 require 'spec_helper'
 describe 'arc' do
   platforms = {
-    'SLED-10.0 i386' => {
-      package_name_default: [ 'tcl', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.0',
-      os: {
-        architecture: 'i386',
-        name: 'SLED',
-        release: {
-          full: '10.0',
-          major: '10',
-        }
-      }
-    },
-    'SLES-10.0 i386' => {
-      package_name_default: [ 'tcl', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.0',
-      os: {
-        architecture: 'i386',
-        name: 'SLES',
-        release: {
-          full: '10.0',
-          major: '10',
-        }
-      }
-    },
-    'SLED-10.0 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.0',
-      os: {
-        architecture: 'x86_64',
-        name: 'SLED',
-        release: {
-          full: '10.0',
-          major: '10',
-        }
-      }
-    },
-    'SLES-10.0 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.0',
-      os: {
-        architecture: 'x86_64',
-        name: 'SLES',
-        release: {
-          full: '10.0',
-          major: '10',
-        }
-      }
-    },
-    'SLED-10.1 i386' => {
-      package_name_default: [ 'tcl', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.1',
-      os: {
-        architecture: 'i386',
-        name: 'SLED',
-        release: {
-          full: '10.1',
-          major: '10',
-        }
-      }
-    },
-    'SLED-10.1 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.1',
-      os: {
-        architecture: 'x86_64',
-        name: 'SLED',
-        release: {
-          full: '10.1',
-          major: '10',
-        }
-      }
-    },
-    'SLED-10.2 i386' => {
-      package_name_default: [ 'tcl', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.2',
-      os: {
-        architecture: 'i386',
-        name: 'SLED',
-        release: {
-          full: '10.2',
-          major: '10',
-        }
-      }
-    },
-    'SLED-10.2 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.2',
-      os: {
-        architecture: 'x86_64',
-        name: 'SLED',
-        release: {
-          full: '10.2',
-          major: '10',
-        }
-      }
-    },
-    'SLED-10.3 i386' => {
-      package_name_default: [ 'tcl', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.3',
-      os: {
-        architecture: 'i386',
-        name: 'SLED',
-        release: {
-          full: '10.3',
-          major: '10',
-        }
-      }
-    },
-    'SLED-10.3 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.3',
-      os: {
-        architecture: 'x86_64',
-        name: 'SLED',
-        release: {
-          full: '10.3',
-          major: '10',
-        }
-      }
-    },
-    'SLED-10.4 i386' => {
-      package_name_default: [ 'tcl', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.4',
-      os: {
-        architecture: 'i386',
-        name: 'SLED',
-        release: {
-          full: '10.4',
-          major: '10',
-        }
-      }
-    },
-    'SLED-10.4 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh' ],
-      rndrelease_version_default: 'LMWP 2.4',
-      os: {
-        architecture: 'x86_64',
-        name: 'SLED',
-        release: {
-          full: '10.4',
-          major: '10',
-        }
-      }
-    },
-    'SLED-11.0 i386' => {
-      package_name_default: [ 'tcl', 'tcsh', 'xorg-x11-libXmu' ],
-      rndrelease_version_default: 'LMWP 3.0',
-      os: {
-        architecture: 'i386',
-        name: 'SLED',
-        release: {
-          full: '11.0',
-          major: '11',
-        }
-      }
-    },
-    'SLES-11.0 i386' => {
-      package_name_default: [ 'tcl', 'tcsh', 'xorg-x11-libXmu' ],
-      rndrelease_version_default: 'LMWP 3.0',
-      os: {
-        architecture: 'i386',
-        name: 'SLES',
-        release: {
-          full: '11.0',
-          major: '11',
-        }
-      }
-    },
     'SLED-11.0 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh', 'xorg-x11-libXmu-32bit' ],
       rndrelease_version_default: 'LMWP 3.0',
       os: {
         architecture: 'x86_64',
@@ -187,7 +17,6 @@ describe 'arc' do
       }
     },
     'SLES-11.0 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh', 'xorg-x11-libXmu-32bit' ],
       rndrelease_version_default: 'LMWP 3.0',
       os: {
         architecture: 'x86_64',
@@ -198,44 +27,18 @@ describe 'arc' do
         }
       }
     },
-    'SLED-11.1 i386' => {
-      package_name_default: [ 'tcl', 'tcsh', 'xorg-x11-libXmu' ],
-      rndrelease_version_default: 'LMWP 3.1',
-      os: {
-        architecture: 'i386',
-        name: 'SLED',
-        release: {
-          full: '11.1',
-          major: '11',
-        }
-      }
-    },
     'SLED-11.1 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh', 'xorg-x11-libXmu-32bit' ],
       rndrelease_version_default: 'LMWP 3.1',
       os: {
         architecture: 'x86_64',
         name: 'SLED',
         release: {
           full: '11.1',
-          major: '11',
-        }
-      }
-    },
-    'SLED-11.2 i386' => {
-      package_name_default: [ 'tcl', 'tcsh', 'xorg-x11-libXmu' ],
-      rndrelease_version_default: 'LMWP 3.2',
-      os: {
-        architecture: 'i386',
-        name: 'SLED',
-        release: {
-          full: '11.2',
           major: '11',
         }
       }
     },
     'SLED-11.2 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh', 'xorg-x11-libXmu-32bit' ],
       rndrelease_version_default: 'LMWP 3.2',
       os: {
         architecture: 'x86_64',
@@ -246,20 +49,7 @@ describe 'arc' do
         }
       }
     },
-    'SLED-11.3 i386' => {
-      package_name_default: [ 'tcl', 'tcsh', 'xorg-x11-libXmu' ],
-      rndrelease_version_default: 'LMWP 3.3',
-      os: {
-        architecture: 'i386',
-        name: 'SLED',
-        release: {
-          full: '11.3',
-          major: '11',
-        }
-      }
-    },
     'SLED-11.3 x86_64' => {
-      package_name_default: [ 'tcl-32bit', 'tcsh', 'xorg-x11-libXmu-32bit' ],
       rndrelease_version_default: 'LMWP 3.3',
       os: {
         architecture: 'x86_64',
@@ -294,10 +84,6 @@ describe 'arc' do
               'content' => "#{v[:rndrelease_version_default]}\n",
             },
           )
-        end
-
-        v[:package_name_default]&.each do |package|
-          it { is_expected.to contain_package(package).with_ensure('present') }
         end
       end
     end
